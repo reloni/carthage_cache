e15424b7ec0bc5cc4f73bc17ddc1e6dc2f79dc6b
Maxim Gubin <maxim@kilograpp.com>
2017-11-09 12:34:49 +0300

    Remove excess code

--- utils/environment.go
+++ utils/environment.go
@@ -2,16 +2,11 @@ package utils
 
 import (
 	"flag"
-	"github.com/joho/godotenv"
 	"buildben/carthage_cache/client/environment"
 )
 
 func LoadProcessArguments() {
 	flag.StringVar(&environment.Platform,"platform", "iOS", "Platform. Default is iOS.")
 	flag.Parse()
-}
-
-func LoadEnvDefaults() {
-	godotenv.Load("fastlane/.env.default")
 }
 
